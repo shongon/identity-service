cbda03ff6e798dfe95d044c0d82b99676a4af820
Hong Son <hongsonn539@gmail.com>
2024-12-08 00:26:19 +0700

    chore: ignore null fields when return response

--- src/main/java/identity_service/dto/ApiResponse.java
+++ src/main/java/identity_service/dto/ApiResponse.java
@@ -1,5 +1,6 @@
 package identity_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) // Ignore null fields when return response
 public class ApiResponse <T> {
     private int code;
     private String message;
