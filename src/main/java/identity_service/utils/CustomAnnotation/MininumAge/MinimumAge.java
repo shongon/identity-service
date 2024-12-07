593b4bd54709c0eae2eaff303d0919e7c39c271c
Hong Son <hongsonn539@gmail.com>
2024-12-08 00:10:46 +0700

    chore: create custom annotation to valid DateOfBirth

---
+++ src/main/java/identity_service/utils/CustomAnnotation/MininumAge/MinimumAge.java
@@ -0,0 +1,19 @@
+package identity_service.utils.CustomAnnotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = MinimumAgeValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MinimumAge {
+    int value(); // Tuổi tối thiểu
+    String message() default "User must be at least {value} years old";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/identity_service/utils/CustomAnnotation/MininumAge/MinimumAgeValidator.java
@@ -0,0 +1,27 @@
+package identity_service.utils.CustomAnnotation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class MinimumAgeValidator implements ConstraintValidator<MinimumAge, LocalDate> {
+
+    private int minimumAge;
+
+    @Override
+    public void initialize(MinimumAge constraintAnnotation) {
+        this.minimumAge = constraintAnnotation.value();
+    }
+
+    @Override
+    public boolean isValid(LocalDate dob, ConstraintValidatorContext context) {
+        if (dob == null) {
+            return true; // Cho phép null, sử dụng @NotNull để kiểm tra riêng
+        }
+
+        // Tính tuổi hiện tại
+        return Period.between(dob, LocalDate.now()).getYears() >= minimumAge;
+    }
+}
