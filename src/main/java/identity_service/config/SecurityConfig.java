ed3aa34130545fca4ae54b007bd4271202232c75
Hong Son <hongsonn539@gmail.com>
2024-12-13 23:55:48 +0700

    feat(security): configure SecurityConfig & Authentication endpoints

--- src/main/java/identity_service/config/SecurityConfig.java
+++ src/main/java/identity_service/config/SecurityConfig.java
@@ -1,4 +1,51 @@
 package identity_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    private final String[] PUBLIC_ENDPOINTS = {
+            "users/register",
+            "auth/login",
+            "auth/introspect",
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf().disable()
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).permitAll()
+                                .anyRequest().authenticated());
+
+        httpSecurity.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())));
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
 }
