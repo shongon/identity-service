8e106d835fce7b79c38631ee8e65aa1c840aed97
Hong Son <hongsonn539@gmail.com>
2024-12-14 23:55:32 +0700

    feat: auto create a Admin account when program first start

--- src/main/java/identity_service/config/AppInitConfig.java
+++ src/main/java/identity_service/config/AppInitConfig.java
@@ -1,4 +1,46 @@
 package identity_service.config;
 
-public class AppInitConfog {
+
+import identity_service.model.User;
+import identity_service.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class AppInitConfig {
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    ApplicationRunner applicationRunner(UserRepository userRepository){
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()){
+
+                User user = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .roles(User.Role.ADMIN)
+                        .firstName("Admin") // Thêm các trường bắt buộc
+                        .lastName("System")
+                        .dob(LocalDate.of(1990, 1, 1)) // Thêm ngày sinh
+                        .createdAt(LocalDateTime.now())
+                        .build();
+
+                userRepository.save(user);
+                log.warn("admin user has been created with default password: admin, please change it");
+            }
+        };
+    }
 }
